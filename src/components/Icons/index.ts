3392bb25a5e7994d5795627c1461f1678353bb65
taehongkim <public.bluemiv@gmail.com>
2024-04-16 12:46:16 +0900

    feat: layout 수정

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,11 +1,9 @@
 import StarIcon from './StarIcon';
 import GithubIcon from './GithubIcon';
-import TistoryIcon from './TistoryIcon';
 
 const Icons = {
   Star: StarIcon,
   Github: GithubIcon,
-  Tistory: TistoryIcon,
 };
 
 export default Icons;
